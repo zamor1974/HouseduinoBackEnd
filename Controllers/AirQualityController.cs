b0b178f9cbc0c95f0917353b60d3075bdf700511
zamor1974 <luigi.amoroso@gmail.com>
2022-12-14 14:58:41 +0100

    test

---
+++ Controllers/AirQualityController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using HouseduinoBackEnd.Helper;
+using HouseduinoBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace HouseduinoBackEnd.Controllers
+{
+    [ApiController]
+    [Route("airquality")]
+    public class AirQualityController : ControllerBase
+    {
+        private readonly ILogger<AirQualityController> _logger;
+
+
+        public AirQualityController(ILogger<AirQualityController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Ritorna la lista di valori della qualita dell'aria
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">Operazione effettuata</response>
+        /// <response code="400">Errore</response>
+        [ProducesResponseType(typeof(ResponseObject), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(500)]
+        [HttpGet("all")]
+        public ResponseObject GetAll()
+        {
+            var response = new ResponseObject();
+            var db = new AirQualityDbHelper();
+            response = db.GetAll().Result;
+
+            return response;
+        }
+
+        /// <summary>
+        /// Ritorna i valori della qualita dell'aria registrate nell'ultima ora
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">Operazione effettuata</response>
+        /// <response code="400">Errore</response>
+        [ProducesResponseType(typeof(ResponseObject), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(500)]
+        [HttpGet("lasthour")]
+        public ResponseObject GetLastHour()
+        {
+            var response = new ResponseObject();
+            var db = new AirQualityDbHelper();
+            response = db.GetLastHour().Result;
+
+            return response;
+        }
+
+        /// <summary>
+        /// Inserisce un valore della qualita dell'aria
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">Operazione effettuata</response>
+        /// <response code="400">Errore</response>
+        [ProducesResponseType(typeof(ResponseObjectInsert), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(500)]
+        [HttpPost("insert")]
+        public ResponseObjectInsert Insert(RequestObject request)
+        {
+            var response = new ResponseObjectInsert();
+            var db = new AirQualityDbHelper();
+            response = db.Insert(request).Result;
+
+            return response;
+        }
+
+        /// <summary>
+        /// Ritorna la lista di n valori della qualita dell'aria
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">Operazione effettuata</response>
+        /// <response code="400">Errore</response>
+        [ProducesResponseType(typeof(ResponseObject), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(500)]
+        [HttpGet("showdata/{recordNumber}")]
+        public ResponseObject GetShowData(int recordNumber)
+        {
+            var response = new ResponseObject();
+            var db = new AirQualityDbHelper();
+            response = db.GetShowData(recordNumber).Result;
+
+            return response;
+        }
+    }
+
+}
+
---
+++ Controllers/LightnessController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using HouseduinoBackEnd.Helper;
+using HouseduinoBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace HouseduinoBackEnd.Controllers
+{
+    [ApiController]
+    [Route("lightness")]
+    public class LightnessController : ControllerBase
+    {
+        private readonly ILogger<LightnessController> _logger;
+
+
+        public LightnessController(ILogger<LightnessController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Ritorna la lista di luminosita
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">Operazione effettuata</response>
+        /// <response code="400">Errore</response>
+        [ProducesResponseType(typeof(ResponseObject2), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(500)]
+        [HttpGet("all")]
+        public ResponseObject2 GetAll()
+        {
+            var response = new ResponseObject2();
+            var db = new LightnessDbHelper();
+            response = db.GetAll().Result;
+
+            return response;
+        }
+
+        /// <summary>
+        /// Ritorna le luminosita registrate nell'ultima ora
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">Operazione effettuata</response>
+        /// <response code="400">Errore</response>
+        [ProducesResponseType(typeof(ResponseObject2), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(500)]
+        [HttpGet("lasthour")]
+        public ResponseObject2 GetLastHour()
+        {
+            var response = new ResponseObject2();
+            var db = new LightnessDbHelper();
+            response = db.GetLastHour().Result;
+
+            return response;
+        }
+
+        /// <summary>
+        /// Inserisce una luminosita
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">Operazione effettuata</response>
+        /// <response code="400">Errore</response>
+        [ProducesResponseType(typeof(ResponseObject2Insert), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(500)]
+        [HttpPost("insert")]
+        public ResponseObjectInsert Insert(RequestObject2 request)
+        {
+            var response = new ResponseObject2Insert();
+            var db = new LightnessDbHelper();
+            response = db.Insert(request).Result;
+
+            return response;
+        }
+
+        /// <summary>
+        /// Ritorna la lista di n luminosita
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">Operazione effettuata</response>
+        /// <response code="400">Errore</response>
+        [ProducesResponseType(typeof(ResponseObject2), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(500)]
+        [HttpGet("showdata/{recordNumber}")]
+        public ResponseObject2 GetShowData(int recordNumber)
+        {
+            var response = new ResponseObject2();
+            var db = new LightnessDbHelper();
+            response = db.GetShowData(recordNumber).Result;
+
+            return response;
+        }
+    }
+
+}
+
