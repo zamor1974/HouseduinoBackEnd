90e138183df82f16f22e1070b03308e36cf93b53
zamor1974 <luigi.amoroso@gmail.com>
2023-01-02 09:33:52 +0100

    test

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 dotnet run
 dotnet watch run
 
+docker list
 docker build -t houseduino_be:0.0.1 -f Dockerfile .
-
+docker save houseduinobackend:0.1.6 > houseduino_be.tar
+docker run -p 5000:80 -t houseduinobackend:0.1.6
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace HouseduinoBackEnd
@@ -39,10 +32,10 @@ namespace HouseduinoBackEnd
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HouseduinoBackEnd v1"));
             }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HouseduinoBackEnd v1"));
 
             app.UseHttpsRedirection();
 
